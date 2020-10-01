57cdac07c033b34329ddae39d6f8b03905794948
Clayton Siby <claytonsiby@gmail.com>
2020-10-01 21:50:37 +0200

    Update routes, and refactor session views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  delete '/sign_out', to: 'sessions#destroy'
+  resources :sessions, only: %i[ new create destroy ]
   resources :users, only: %i[new create show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'users#new'
 end
