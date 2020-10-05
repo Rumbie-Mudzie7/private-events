4abaf320f24b97bc477abec0910ed1b4f2e22e12
rumbie <rumbiemudzie@gmail.com>
2020-10-05 20:46:48 +0200

    Migrate initial Event model to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_112840) do
+ActiveRecord::Schema.define(version: 2020_10_05_183937) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "venue"
+    t.text "description"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
