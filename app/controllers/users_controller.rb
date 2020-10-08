e56d69e84b69dae94801f59a76ce95d4e62752f4
Clayton Siby <claytonsiby@gmail.com>
2020-10-08 21:13:19 +0200

    Update Events#Index page to show all events (upcoming/previous)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     # @user = User.find(params[:id])
     @user = current_user
     @created_events = @user.created_events
-    # @previously_attended_events = current_user.attended_events.previously_events
+    # @previously_attended_events = current_user.attended_events.previous_events
     # @upcoming_events = current_user.attended_events.upcoming_events
   end
 
