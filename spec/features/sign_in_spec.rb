f98a2a056e44efbe80a80681ec0bacebde9a8556
Clayton Siby <claytonsiby@gmail.com>
2020-10-12 16:11:48 +0200

    Write rspec tests for signing in

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign In', type: :feature do
+  before(:each) do
+    @user = User.create( name: 'John', email: 'johndoe@gmail.com', password: '1234')
+  end
+  
+  scenario 'User successfully Signed In' do
+    visit new_session_path
+    fill_in 'Email', with: @user.email
+    click_button 'Sign In'
+    expect(page).to have_content("SignedIn As: #{@user.name}")
+  end
+end
