f4612a6f82f234f7589f06569fc61d456479df5f
rumbie <rumbiemudzie@gmail.com>
2020-10-12 21:50:48 +0200

    Integrate sign_in and sign_out spec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+  validates :password, presence: true
 
     has_many :event_attendings, foreign_key: :attendee_id
     has_many :attended_events, through: :event_attendings
